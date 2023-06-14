50940e99f66349bffe904258b2559f908528ed77
rucha1128 <rucha281999@gmail.com>
2023-06-14 17:49:50 +0530

    Add categories UI

--- BookShop/BookShop/Controllers/CategoriesController.cs
+++ BookShop/BookShop/Controllers/CategoriesController.cs
@@ -1,16 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookShop.Data;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Controllers
 {
     public class CategoriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Categories> objCategory = _db.Categories.ToList();
+            return View(objCategory);
         }
-        public IActionResult Categories()
+        public IActionResult AddCategory()
         {
-            return this.View();
+            return View();
         }
     }
 }
--- BookShop/BookShop/Data/ApplicationDbContext.cs
+++ BookShop/BookShop/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace BookShop.Data
         }
 
         public DbSet<Categories> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>().HasData(
+                new Categories { id = 1, Name = "History", DisplayOrder = 1},
+                new Categories { id = 2, Name = "Literature", DisplayOrder = 2 },
+                new Categories { id = 3, Name = "Novel", DisplayOrder = 3 }
+                );
+        }
     }
 }
