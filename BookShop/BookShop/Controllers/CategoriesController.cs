c21b999e8c3813d8ac2516cdbb68bce0580d22ad
rucha1128 <rucha281999@gmail.com>
2023-06-16 17:37:16 +0530

    Edit and Delete functionality. Toastr Notifications

--- BookShop/BookShop/Controllers/CategoriesController.cs
+++ BookShop/BookShop/Controllers/CategoriesController.cs
@@ -28,11 +28,17 @@ namespace BookShop.Controllers
                 _db.Categories.Add(cat);
                 _db.SaveChanges();
             }
-            return View();
+            TempData["success"] = "Category Created Successfully !";
+            return RedirectToAction("Index");
         }
-        public IActionResult Edit()
+        public IActionResult Edit(int? id)
         {
-            return View();
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.FirstOrDefault(u => u.id == id);
+            return View(categoryFromDb);
         }
         [HttpPost]
         public IActionResult Edit(Categories cat)
@@ -42,6 +48,24 @@ namespace BookShop.Controllers
                 _db.Categories.Update(cat);
                 _db.SaveChanges();
             }
+            TempData["success"] = "Category Updated Successfully !";
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.FirstOrDefault(u => u.id == id);
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Delete(Categories cat)
+        {
+                _db.Categories.Remove(cat);
+                _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully !";
             return RedirectToAction("Index");
         }
     }
