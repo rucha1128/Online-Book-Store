3ef3069eb0cdf9672dad9bc6dbb579cbecb62731
Rucha <rucha281999@gmail.com>
2023-07-09 13:29:02 +0530

    Project Reference DLL added

--- BookShop/BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
+++ BookShop/BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21b999e8c3813d8ac2516cdbb68bce0580d22ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
